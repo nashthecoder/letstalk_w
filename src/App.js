b20e8f63787eb320ba687bc5966f996b9491de13
NashTheCoder <naijeria@gmail.com>
2020-11-14 22:17:41 +0300

    installed react-router-dom and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import SearchPage from './components/SearchPage';
+import Chat from './components/Chat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/profile'>
+            <Profile />
+            <Chat />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header';
+import './Header.css';
+
+
+function Header() {
+    return (
+        <div className='header'>
+                <h3>Testing</h3>
+                <img className='header__logo' src='/public/images/lets_talk_logo.png' alt=''/>
+        </div>
+    )
+}
+
+export default Header
