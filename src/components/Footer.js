ed8ab809314940657a4f48b5bf8b8c8120e964ea
NashTheCoder <naijeria@gmail.com>
2020-11-16 04:34:48 +0300

    added footer code and css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div>
-            <h3>Footer</h3>
+            <div classNameName='footer'>
+            <ul className="footer__grid__container">
+                <li className="footer__item"><a href="#" className="footer__link">About</a></li>
+                <li className="footer__item"><a href="#" className="footer__link">Help Center</a></li>
+                <li className="footer__item"><a href="#" className="footer__link">Terms</a></li>
+                <li className="footer__item"><a href="#" className="footer__link">Privacy policy</a></li>
+                <li className="footer__item"><a href="#" className="footer__link">Cookies</a></li>
+                <li className="footer__item">
+                <p className="footer__item">&copy; 2020 NashTheCoder.</p>
+                </li>
+            </ul>
         </div>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -30,18 +30,6 @@ function Login() {
                     </div>
                     </div>
                 </section>
-                <div classNameName='footer'>
-            <ul className="footer__grid__container">
-                <li className="footer__item"><a href="#" className="footer__link">About</a></li>
-                <li className="footer__item"><a href="#" className="footer__link">Help Center</a></li>
-                <li className="footer__item"><a href="#" className="footer__link">Terms</a></li>
-                <li className="footer__item"><a href="#" className="footer__link">Privacy policy</a></li>
-                <li className="footer__item"><a href="#" className="footer__link">Cookies</a></li>
-                <li className="footer__item">
-                <p className="footer__item">&copy; 2020 NashTheCoder.</p>
-                </li>
-            </ul>
-        </div>
         </div>
     )
 }
